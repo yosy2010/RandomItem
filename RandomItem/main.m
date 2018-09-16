cb9e2f25d7a198f8924e7a476fea2fee881b970e
Yasser Altamimi <yosy2010@gmail.com>
2018-09-16 20:48:59 +0300

    Initial Commit

---
+++ RandomItem/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  RandomItem
+//
+//  Created by YASSER ALTAMIMI on 16/09/2018.
+//  Copyright © 2018 YASSER ALTAMIMI. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
